339a398585f2a9b34e5becd49037f2343fd7d0c0
Alexander Poole <alexanderp@opera.com>
2025-05-05 06:57:11 +0200

    Fix mouse buttons

--- keyboards/splitkb/halcyon/elora/keymaps/owodunni/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/owodunni/keymap.c
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_GRV  , KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,   KC_5 ,                                           KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_MINS ,
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                           KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSLS,
      KC_BSLS , KC_A ,  GUI_S  ,  ALT_D ,   CTL_F,   KC_G ,                                           KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B ,   MS_RGHT, MS_BTN5,    FKEYS  , KC_RBRC, GUI_N,  CTL_M , KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                ADJUST , MS_BTN8,   NAV_ESC, SYM_SPC, MS_BTN1,    MS_BTN2, SYM_ENT, KC_BSPC,  KC_RGUI, KC_APP,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B ,   MS_BTN2, MS_BTN3,    FKEYS  , KC_RBRC, GUI_N,  CTL_M , KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+                                ADJUST , MS_BTN4,   NAV_ESC, SYM_SPC, MS_BTN1,    MS_BTN2, SYM_ENT, KC_BSPC,  KC_RGUI, KC_APP,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
